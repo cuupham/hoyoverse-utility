3ea4574c60bff5bf6f0ea74872db15b21367d0a3
cuupham <cuu9991@gmail.com>
2025-11-30 11:37:05 +0700

    test env 7

--- test_read_env.py
+++ test_read_env.py
@@ -1,27 +1,4 @@
 import os
 
-def main():
-    # Lấy secret từ env (GitHub Actions inject vào)
-    cookie = os.getenv("HOYOVERSE_COOKIE")
-    
-    if cookie:
-        print("Cookie đã load thành công!")
-        # Không in nguyên cookie ra log nếu là secret thật
-        # Nếu muốn test, có thể in 1 phần nhỏ
-        print("First 50 chars:", cookie[:50])
-    else:
-        print("Chưa có cookie trong env")
-
-
-    cookie = os.getenv("PATH")
-    
-    if cookie:
-        print("Cookie đã load thành công!")
-        # Không in nguyên cookie ra log nếu là secret thật
-        # Nếu muốn test, có thể in 1 phần nhỏ
-        print("First 50 chars:", cookie[:50])
-    else:
-        print("Chưa có cookie trong env")
-
-if __name__ == "__main__":
-    main()
+for key, value in os.environ.items():
+    print(f"{key}={value}")
