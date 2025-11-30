47244770e428d69e888bafe67723bc0c1352bcd0
cuupham <cuu9991@gmail.com>
2025-11-30 11:31:53 +0700

    test env 4

--- test_read_env.py
+++ test_read_env.py
@@ -12,5 +12,14 @@ def main():
     else:
         print("Chưa có cookie trong env")
 
+
+    cookie = os.getenv("Path")
+    
+    if cookie:
+        print("Cookie đã load thành công!")
+        # Không in nguyên cookie ra log nếu là secret thật
+        # Nếu muốn test, có thể in 1 phần nhỏ
+        print("First 50 chars:", cookie[:50])
+    else:
 if __name__ == "__main__":
     main()
