94c4a05f75d6784bd30f69dde48bb26e3615b5d9
cuupham <cuu9991@gmail.com>
2025-12-01 07:06:09 +0700

    feat: add threading

--- daily_check_in.py
+++ daily_check_in.py
@@ -2,22 +2,23 @@ from helper import parse_cookies, cookies_env
 from flows.gs_flow import GsFlow
 from flows.sr_flow import SrFlow
 from flows.zzz_flow  import ZzzFlow
+import threading
 
 
-def process_all_checkin(cookies_value: str):
+def process_all_checkin(cookie_name: str, cookies_value: str):
+    
     parsed_cookies  = parse_cookies(cookies_value)
     gs = GsFlow(parsed_cookies)
     sr = SrFlow(parsed_cookies)
     zzz = ZzzFlow(parsed_cookies)
 
-    print('[Genshin]:\t', end='')
-    gs.process_checkin()
+    gs_result = gs.process_checkin()
+    sr_result = sr.process_checkin()
+    zzz_result = zzz.process_checkin()
 
-    print('[Star Rail]:\t', end='')
-    sr.process_checkin()
-
-    print('[ZZZ]:\t\t', end='')
-    zzz.process_checkin()
+    prologue = f'------ {cookie_name} ------'
+    message = f'{prologue}\n[Genshin]:\n\t{gs_result}[Star Rail]:\n\t{sr_result}[ZZZ]:\n\t{zzz_result}\n{'-'*len(prologue)}'
+    print(message)
 
 def main():
     all_cookies = cookies_env()
@@ -27,10 +28,9 @@ def main():
         return
 
     for cookie_name, cookies_value in all_cookies.items():
-        prologue = f'------ {cookie_name} ------'
-        print(prologue)
-        process_all_checkin(cookies_value)
-        print('-' * len(prologue) + '\n')
+        threading.Thread(target=process_all_checkin, args=(cookie_name, cookies_value,)).start()
+        #process_all_checkin(cookie_name, cookies_value)
+        
 
 if __name__ == "__main__":
     main()
--- flows/base_flow.py
+++ flows/base_flow.py
@@ -2,7 +2,7 @@ class BaseFlow:
     def __init__(self, api_class, cookies: dict):
         self.api = api_class(cookies)
             
-    def process_checkin(self):
+    def process_checkin(self) -> str:
         info = self.api.info()
 
         retcode = info['retcode']
@@ -10,14 +10,10 @@ class BaseFlow:
             case 0:
                 is_sign = info['data']['is_sign']
             case -10002:
-                print(info['message'])
-                return
+                return info['message']
 
         if is_sign:
-            print('User has already checked in.')
-            return
+            return 'User has already checked in.'
 
-        response = self.api.checkin()
-        print(response)
-
+        return self.api.checkin()
 
