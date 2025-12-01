7f63e948799ba7919a2243218ea7d654207fa229
cuupham <cuu9991@gmail.com>
2025-12-01 07:38:55 +0700

    update: change message response

--- daily_check_in.py
+++ daily_check_in.py
@@ -6,7 +6,6 @@ import threading
 
 
 def process_all_checkin(cookie_name: str, cookies_value: str):
-    
     parsed_cookies  = parse_cookies(cookies_value)
     gs = GsFlow(parsed_cookies)
     sr = SrFlow(parsed_cookies)
@@ -16,8 +15,16 @@ def process_all_checkin(cookie_name: str, cookies_value: str):
     sr_result = sr.process_checkin()
     zzz_result = zzz.process_checkin()
 
-    prologue = f'------ {cookie_name} ------'
-    message = f'{prologue}\n[Genshin]:\n\t{gs_result}\n[Star Rail]:\n\t{sr_result}\n[ZZZ]:\n\t{zzz_result}\n{'-'*len(prologue)}'
+    prologue = f"------ {cookie_name} ------"
+    epilogue = "-" * len(prologue)
+    message = (
+        f"{prologue}\n"
+        f"• Genshin:\n→ {gs_result}\n"
+        f"• Star Rail:\n→ {sr_result}\n"
+        f"• ZZZ:\n→ {zzz_result}\n"
+        f"{epilogue}\n\n"
+    )
+
     print(message)
 
 def main():
@@ -29,7 +36,6 @@ def main():
 
     for cookie_name, cookies_value in all_cookies.items():
         threading.Thread(target=process_all_checkin, args=(cookie_name, cookies_value,)).start()
-        #process_all_checkin(cookie_name, cookies_value)
         
 
 if __name__ == "__main__":
