5b506aec95944803c3daf54c8db61c4296bf4a7a
cuupham <cuu9991@gmail.com>
2025-12-06 09:14:19 +0700

    change: update name in step job

--- auto_checkin.py
+++ auto_checkin.py
@@ -20,7 +20,7 @@ def process_checkin(c_name: str, c_value: str):
         f"------ {c_name} ------\n"
         f"[Genshin]:\n• {gs_result}\n"
         f"[Star Rail]:\n• {sr_result}\n"
-        f"[ZZZ]:\n• {zzz_result}\n"
+        f"[ZZZ]:\n• {zzz_result}\n\n"
     )
     print(message)
 
